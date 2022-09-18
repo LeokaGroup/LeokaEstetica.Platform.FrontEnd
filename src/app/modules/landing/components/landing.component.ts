2e4c80b09a334bf8bf6e5e836b782dd6bc5ba750
Oyarsa <sierra_93@mail.ru>
2022-09-18 03:14:19 +0300

    [#2zaa4n7] Добавил страницу лендоса.

---
+++ src/app/modules/landing/components/landing.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "landing",
+    templateUrl: "./landing.component.html",
+    styleUrls: ["./landing.component.scss"]
+})
+
+/**
+ * Класс календаря пользователя.
+ */
+export class LandingComponent implements OnInit {
+    constructor() {
+    }
+
+    public ngOnInit(): void {
+
+    }
+}
