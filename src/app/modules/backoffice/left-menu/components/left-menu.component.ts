7f9da61a60951f506165fc8d414f2225cecae5de
Oyarsa <sierra_93@mail.ru>
2022-10-19 22:29:50 +0300

    Правки по верстке ЛК.

--- src/app/modules/backoffice/left-menu/components/left-menu.component.ts
+++ src/app/modules/backoffice/left-menu/components/left-menu.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
 import { BackOfficeService } from "../../services/backoffice.service";
 
 @Component({
@@ -14,54 +13,10 @@ import { BackOfficeService } from "../../services/backoffice.service";
 export class LeftMenuComponent implements OnInit {
     public readonly profileItems$ = this._backOfficeService.profileItems$;
 
-    constructor(private readonly _router: Router,
-        private readonly _backOfficeService: BackOfficeService) {
+    constructor(private readonly _backOfficeService: BackOfficeService) {
     }
 
-    aProfileMenuItems = [
-        {
-            label: "Анкета",
-            items: [
-                {label: "Просмотр анкеты"},
-                {label: "Изменить анкету"},
-                {label: "Удалить анкету"}
-            ]
-        },
-        {
-            label: "Мои проекты",
-            items: [
-                {label: "Мои проекты"},
-                {label: "Проекты в архиве"}
-            ]
-        },
-        {
-            label: "Команды", sysName: "Test"
-        },
-        {
-            label: "Приглашения"
-        },
-        {
-            label: "Баланс"
-        },
-        {
-            label: "Сообщения"
-        },
-        {
-            label: "Подписки"
-        },
-        {
-            label: "Тарифы"
-        },
-        {
-            label: "Настройки"
-        },
-        {
-            label: "Выйти"
-        }
-    ];
-
     public async ngOnInit() {
-        console.log(this.aProfileMenuItems);
         await this.getProfileInfoAsync();
     }
 
