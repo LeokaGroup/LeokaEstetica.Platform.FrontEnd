2fc1f665ae46d8602c3657dc246b02b639afda88
Oyarsa <sierra_93@mail.ru>
2022-11-14 22:22:06 +0300

    #107 Добавил страницу создания вакансии.

---
+++ src/app/modules/vacancy/create/create.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { UserService } from "../../user/services/user.service";
+
+@Component({
+    selector: "create",
+    templateUrl: "./create.component.html",
+    styleUrls: ["./create.component.scss"]
+})
+
+/**
+ * Класс компонента создания вакансии.
+ */
+export class CreateVacancyComponent implements OnInit {
+    constructor(private readonly _userService: UserService,
+        private readonly _router: Router) { }
+    public readonly userData$ = this._userService.userData$;
+
+    vacancyName: string = "";
+    vacancyText: string = "";
+
+    public async ngOnInit() {
+        
+    };
+
+   /**
+     * Функция регистрирует пользователя.     
+     * @returns - Данные пользователя.
+     */
+    // public async onSendFormSignUpAsync() {    
+    //     (await this._userService.signUpAsync(this.formSignUp.value.email, this.formSignUp.value.password))
+    //     .subscribe(_ => {
+    //         console.log("Новый пользователь: ", this.userData$.value);
+    //         this._router.navigate(["/user/signin"]);
+    //     });
+    // };
+}
--- src/app/modules/vacancy/vacancy.module.ts
+++ src/app/modules/vacancy/vacancy.module.ts
@@ -1,22 +1,42 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { VacancyRoutingModule } from './vacancy-routing.module';
 import { MessageService } from 'primeng/api';
 import { ToastModule } from 'primeng/toast';
 import { UserService } from '../user/services/user.service';
+import { CatalogVacancyComponent } from './catalog/components/catalog-vacancy.component';
+import { CreateVacancyComponent } from './create/create.component';
+import { PanelMenuModule } from 'primeng/panelmenu';
+import { EditorModule } from 'primeng/editor';
+import { MenuModule } from 'primeng/menu';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { InputMaskModule } from 'primeng/inputmask';
+import { CheckboxModule } from 'primeng/checkbox';
+import { InputTextModule } from 'primeng/inputtext';
+import { PanelModule } from 'primeng/panel';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
     declarations: [
-        
+        CatalogVacancyComponent,
+        CreateVacancyComponent
     ],
 
     imports: [
         CommonModule,
         FormsModule,
-        VacancyRoutingModule,
-        ReactiveFormsModule,
-        ToastModule
+        HttpClientModule,
+        PanelModule,
+        MenuModule,
+        InputTextModule,
+        CheckboxModule,
+        InputMaskModule,
+        InputTextareaModule,
+        PanelMenuModule,
+        ToastModule,
+        EditorModule,
+        VacancyRoutingModule
     ],
 
     exports: [],
