64a9da36691d08fa5174795689e142a562d486b2
Timoshenko A.I <sierra_93@mail.ru>
2023-02-19 21:02:25 +0300

    Revert

--- src/app/modules/project/detail/components/detail.component.ts
+++ src/app/modules/project/detail/components/detail.component.ts
@@ -87,7 +87,9 @@ export class DetailProjectComponent {
     selectedInviteVacancy: any;
     selectedInviteUser: string = "";
     isDeleteProject: boolean = false;
+    isOwner: boolean = false;
     isDeleteVacancyInProject: boolean = false;
+
     public async ngOnInit() {
         forkJoin([
         this.checkUrlParams(),
@@ -160,6 +162,8 @@ export class DetailProjectComponent {
         (await this._projectService.getProjectAsync(projectId, mode))
         .subscribe(_ => {
             console.log("Получили проект: ", this.selectedProject$.value);
+            this.selectedStage = this.selectedProject$.value;
+            this.isOwner = this.selectedProject$.value.isVisibleAction;
         });
     };
 
