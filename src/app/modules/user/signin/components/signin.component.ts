f7a954ef8d8aac1f127c7a252493b2c0bb67b411
mossoii84 <mossomkh@gmail.com>
2023-02-19 19:24:28 +0100

    Вход на сайти и регистрацию - вспылвающие окна работают

--- src/app/modules/user/signin/components/signin.component.ts
+++ src/app/modules/user/signin/components/signin.component.ts
@@ -32,34 +32,30 @@ export class SignInComponent implements OnInit {
 
     public readonly userData$ = this._userService.userData$;
 
-    public async ngOnInit() {
-
-    };
+    public async ngOnInit() {};
 
      /**
-     * Функция регистрирует пользователя.     
+     * Функция регистрирует пользователя.
      * @returns - Данные пользователя.
      */
-      public async onSendFormSignInAsync() {    
+      public async onSendFormSignInAsync() {
         (await this._userService.signInAsync(this.formSignUp.value.email, this.formSignUp.value.password))
         .subscribe((response: any) => {
             console.log("Авторизовались: ", this.userData$.value);
             if (this.userData$.value.errors == null) {
                 localStorage["t_n"] = this.userData$.value.token;
                 localStorage["u_c"] = this.userData$.value.userCode;
-                
                 this._router.navigate(["/profile/aboutme"], {
                     queryParams: {
                         mode: "view"
                     }
                 });
             }
-
             else {
                 console.log("errors validate", response);
                 response.errors.forEach((item: any) => {
                     this._messageService.add({ severity: 'error', summary: "Что то не так", detail: item.errorMessage });
-                });                
+                });
             }
         });
     };
--- src/app/modules/user/signup/components/signup.component.ts
+++ src/app/modules/user/signup/components/signup.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
 import { UserService } from "../../services/user.service";
+import {MessageService} from "primeng/api";
 
 @Component({
     selector: "signup",
@@ -14,15 +15,13 @@ import { UserService } from "../../services/user.service";
  */
 export class SignUpComponent implements OnInit {
     constructor(private readonly _userService: UserService,
-        private readonly _router: Router) { }
+        private readonly _router: Router,private readonly _messageService: MessageService) { }
 
     formSignUp: FormGroup = new FormGroup({
-
         "email": new FormControl("", [
             Validators.required,
             Validators.email
         ]),
-
         "password": new FormControl("", [
             Validators.required,
             Validators.pattern(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*#?&^_-]).{8,}/)
@@ -31,19 +30,26 @@ export class SignUpComponent implements OnInit {
 
     public readonly userData$ = this._userService.userData$;
 
-    public async ngOnInit() {
+    public async ngOnInit() {};
 
-    };
 
-   /**
-     * Функция регистрирует пользователя.     
-     * @returns - Данные пользователя.
-     */
-    public async onSendFormSignUpAsync() {    
-        (await this._userService.signUpAsync(this.formSignUp.value.email, this.formSignUp.value.password))
-        .subscribe(_ => {
-            console.log("Новый пользователь: ", this.userData$.value);
-            this._router.navigate(["/user/signin"]);
-        });
-    };
+  /**
+   * Функция регистрирует пользователя.
+   * @returns - Данные пользователя.
+   */
+  public async onSendFormSignUpAsync() {
+    (await this._userService.signUpAsync(this.formSignUp.value.email, this.formSignUp.value.password))
+      .subscribe((response: any) =>  {
+        if (this.userData$.value.errors == null) {
+          console.log("Новый пользователь: ", this.userData$.value);
+          this._router.navigate(["/user/signin"]);
+        }
+        else {
+          console.log("errors validate", response);
+          response.errors.forEach((item: any) => {
+            this._messageService.add({ severity: 'error', summary: "Что то не так", detail: item.errorMessage });
+          });
+        }
+      });
+  }
 }
