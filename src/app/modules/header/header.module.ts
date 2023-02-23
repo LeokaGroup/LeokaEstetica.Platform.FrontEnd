ffd8c0aff5c7c5f2c9ba56f893ba3b0b44c46789
mossoii84 <mossomkh@gmail.com>
2023-02-23 15:01:52 +0100

    настройка нопков Login/avatar при входе

--- src/app/modules/header/header.module.ts
+++ src/app/modules/header/header.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HeaderComponent } from './components/header.component';
 import { HeaderRoutingModule } from './header-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { MenuModule } from 'primeng/menu';
 
 @NgModule({
-    declarations: [
-        HeaderComponent
-    ],
+    declarations: [],
 
     imports: [
         CommonModule,
