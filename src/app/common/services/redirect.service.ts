c6d6d30e841ae8ee04fa991bd807008b3eb0ab3f
Timoshenko A.I <sierra_93@mail.ru>
2023-04-06 10:35:09 +0300

    Убрал лишнюю функцию из редирект сервиса, не использовалась.

--- src/app/common/services/redirect.service.ts
+++ src/app/common/services/redirect.service.ts
@@ -16,19 +16,4 @@ export class RedirectService {
     public redirect(route: string): void {
         window.location.href = window.location.protocol + '//' + window.location.host + '/' + route;
     };
-
-    /**
-     * Функция редиректит по указанному роуту с параметрами в роуте для просмотра проекта.
-     * Важно для уведомлений, не удалять!
-     * Это сокращает обновление страницы для пользователя.  
-     * @param route - Роут, куда надо редиректить.
-     * @param param1 - Первый параметр в роуте.
-     * @param param2 - Второй параметр в роуте.
-     * @param param1Value - Значение для первого параметра в роуте.
-     * @param param2Value - Значение для второго параметра в роуте.
-     */
-    public redirectWithParamsViewProject(route: string, param1Name: string, param2Name: string, param1Value: number, 
-        param2Value: string): void {
-        window.location.href = window.location.protocol + '//' + window.location.host + '/' + route + "?" + param1Name + "=" + param1Value + "&" + param2Name + "=" + param2Value;
-    };
 }
