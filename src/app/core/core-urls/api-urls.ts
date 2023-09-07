c93741378001e1c5a0dbc5eb923f7c63b715e6ff
Timoshenko A.I <sierra_93@mail.ru>
2023-09-07 21:53:55 +0300

    #18686848 Изменил домены бэка.

--- src/app/core/core-urls/api-urls.ts
+++ src/app/core/core-urls/api-urls.ts
@@ -3,9 +3,9 @@
  */
 export const API_URL = {
     apiUrl: window.location.href.includes("https://leoka-estetica-dev.ru") || window.location.href.includes("http://localhost:4200/")
-        ? "leoka-estetica-dev.ru.net"
+        ? "https://leoka-estetica-dev.ru.net"
         // ? "http://localhost:9992"
         : window.location.href.includes("https://leoka-estetica-test.ru")
-            ? "leoka-estetica-test.ru.net"
-            : "leoka-estetica.ru.net"
+            ? "https://leoka-estetica-test.ru.net"
+            : "https://leoka-estetica.ru.net"
 };
