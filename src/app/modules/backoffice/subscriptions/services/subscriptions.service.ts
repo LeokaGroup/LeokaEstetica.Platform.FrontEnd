bc59873b9566c88bb977f766b6d884a1323831e1
Timoshenko A.I <sierra_93@mail.ru>
2023-10-24 22:27:08 +0300

    #23023074 Реализовал логику ручного возврата и убрал автоматические возвраты + уведомления добавил.

--- src/app/modules/backoffice/subscriptions/services/subscriptions.service.ts
+++ src/app/modules/backoffice/subscriptions/services/subscriptions.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, tap } from 'rxjs';
 import { API_URL } from 'src/app/core/core-urls/api-urls';
+import { CreateRefundInput } from '../../vacancy/models/input/create-refund-input';
 
 /**
  * Класс сервиса подписок.
@@ -11,6 +12,7 @@ export class SubscriptionsService {
     public subscriptions$ = new BehaviorSubject<any>(null);
     public refund$ = new BehaviorSubject<any>(null);
     public fareRuleInfo$ = new BehaviorSubject<any>(null);
+    public createdRefund$ = new BehaviorSubject<any>(null);
 
     constructor(private readonly _http: HttpClient) {
 
@@ -43,6 +45,17 @@ export class SubscriptionsService {
      public async getFareRuleDetailsAsync(objectId: number) {
         return await this._http.get(API_URL.apiUrl + "/rules/details?objectId=" + objectId).pipe(
             tap(data => this.fareRuleInfo$.next(data))
+        );
+    };
+
+     /**
+      * TODO: Вынести эту функцию в отдельный сервис возвратов. го тоже надо завести.
+     * Функция создает возврат.
+     * @returns - Данные возврата.
+     */
+     public async createRefundAsync(refundInput: CreateRefundInput) {
+        return await this._http.post(API_URL.apiUrl + "/refunds/refunds", refundInput).pipe(
+            tap(data => this.createdRefund$.next(data))
         );
     };
 }
