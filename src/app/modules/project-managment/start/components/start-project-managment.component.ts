6ad0c04d639bd3b74f71edfef31d0dd6e8a61542
Timoshenko A.I <sierra_93@mail.ru>
2023-11-19 14:11:53 +0300

    #25070697 Получение проектов пользователя.

--- src/app/modules/project-managment/start/components/start-project-managment.component.ts
+++ src/app/modules/project-managment/start/components/start-project-managment.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { forkJoin } from "rxjs";
+import { ProjectManagmentService } from "../../services/project-managment.service";
 
 @Component({
     selector: "",
@@ -11,12 +12,27 @@ import { forkJoin } from "rxjs";
  * Класс модуля управления проектами (начальная страница, не Landing).
  */
 export class StartProjectManagmentComponent implements OnInit {
-    constructor() {
+    public readonly userProjects$ = this._projectManagmentService.userProjects$;
+
+    selectedProject: any;
+
+    constructor(private readonly _projectManagmentService: ProjectManagmentService) {
     }
 
     public async ngOnInit() {
         forkJoin([
-          
+          await this.getUseProjectsAsync()
         ]).subscribe();
+    };
+
+    /**
+  * Функция получает список проектов пользователя.
+  * @returns - Список проектов.
+  */
+    private async getUseProjectsAsync() {
+        (await this._projectManagmentService.getUseProjectsAsync())
+            .subscribe(_ => {
+                console.log("Проекты пользователя для УП: ", this.userProjects$.value.userProjects);
+            });
     };
 }
