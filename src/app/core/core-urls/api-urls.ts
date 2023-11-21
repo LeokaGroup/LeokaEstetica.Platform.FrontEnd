8a0075126229aad0b72d420139c0d40e554dabf0
Timoshenko A.I <sierra_93@mail.ru>
2023-11-21 11:32:22 +0300

    #24754222 Поправил ендпоинты к модулю УП.

--- src/app/core/core-urls/api-urls.ts
+++ src/app/core/core-urls/api-urls.ts
@@ -9,9 +9,9 @@ export const API_URL = {
             ? "https://leoka-estetica-test.ru.net"
             : "https://leoka-estetica.ru.net",
     apiUrlProjectManagment: window.location.href.includes("https://leoka-estetica-dev.ru") || window.location.href.includes("http://localhost:4200/")
-    ? "https://leoka-estetica-dev.ru.net"
+    ? "https://project-managment.leoka-estetica-dev.ru.net"
     // ? "http://localhost:9995"
     : window.location.href.includes("https://leoka-estetica-test.ru")
-        ? "https://leoka-estetica-test.ru.net"
-        : "https://leoka-estetica.ru.net"
+        ? "https://project-managment.leoka-estetica-test.ru.net"
+        : "https://project-managment.leoka-estetica.ru.net"
 };
