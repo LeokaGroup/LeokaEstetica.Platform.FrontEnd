be835dea3be1ed361d908c07392d8daf509957df
Timoshenko A.I <sierra_93@mail.ru>
2023-11-24 01:04:49 +0300

    #25424814 Добавил хидер (пока без биндинга).

--- src/app/modules/project-managment/start/components/space/space.component.ts
+++ src/app/modules/project-managment/start/components/space/space.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 import { forkJoin } from "rxjs";
+import { RedirectService } from "src/app/common/services/redirect.service";
 import { ProjectManagmentService } from "../../../services/project-managment.service";
 
 @Component({
@@ -12,23 +14,185 @@ import { ProjectManagmentService } from "../../../services/project-managment.ser
  * Класс модуля управления проектами (рабочее пространство).
  */
 export class SpaceComponent implements OnInit {
-    constructor(private readonly _projectManagmentService: ProjectManagmentService) {
+    constructor(private readonly _projectManagmentService: ProjectManagmentService,
+        private readonly _router: Router,
+        private readonly _redirectService: RedirectService) {
     }
+    
+    public readonly headerItems$ = this._projectManagmentService.headerItems$;
+
+    isHideAuthButtons: boolean = false;
+
+    items: any[] = [
+        {
+            label: 'Заказы',
+            command: () => {
+                this._router.navigate(["/profile/orders"]);
+            }
+        },
+        // {
+        //     label: 'Настройки',
+        //     command: () => {
+
+        //     }
+        // },       
+        {
+            label: 'Заявки в поддержку',
+            command: () => {
+                this._router.navigate(["/profile/tickets"])
+            }
+        },   
+        {
+            label: 'Выйти',
+            command: () => {
+                localStorage.clear();
+                this._router.navigate(["/user/signin"]).then(() => {  
+                    this._redirectService.redirect("user/signin");                
+                });
+            }
+        }
+    ];
+
+    items1: any[] = [
+        {
+            label: 'File',
+            icon: 'pi pi-fw pi-file',
+            items: [
+                {
+                    label: 'New',
+                    icon: 'pi pi-fw pi-plus',
+                    items: [
+                        {
+                            label: 'Bookmark',
+                            icon: 'pi pi-fw pi-bookmark'
+                        },
+                        {
+                            label: 'Video',
+                            icon: 'pi pi-fw pi-video'
+                        }
+                    ]
+                },
+                {
+                    label: 'Delete',
+                    icon: 'pi pi-fw pi-trash'
+                },
+                {
+                    separator: true
+                },
+                {
+                    label: 'Export',
+                    icon: 'pi pi-fw pi-external-link'
+                }
+            ]
+        },
+        {
+            label: 'Edit',
+            icon: 'pi pi-fw pi-pencil',
+            items: [
+                {
+                    label: 'Left',
+                    icon: 'pi pi-fw pi-align-left'
+                },
+                {
+                    label: 'Right',
+                    icon: 'pi pi-fw pi-align-right'
+                },
+                {
+                    label: 'Center',
+                    icon: 'pi pi-fw pi-align-center'
+                },
+                {
+                    label: 'Justify',
+                    icon: 'pi pi-fw pi-align-justify'
+                }
+            ]
+        },
+        {
+            label: 'Users',
+            icon: 'pi pi-fw pi-user',
+            items: [
+                {
+                    label: 'New',
+                    icon: 'pi pi-fw pi-user-plus'
+                },
+                {
+                    label: 'Delete',
+                    icon: 'pi pi-fw pi-user-minus'
+                },
+                {
+                    label: 'Search',
+                    icon: 'pi pi-fw pi-users',
+                    items: [
+                        {
+                            label: 'Filter',
+                            icon: 'pi pi-fw pi-filter',
+                            items: [
+                                {
+                                    label: 'Print',
+                                    icon: 'pi pi-fw pi-print'
+                                }
+                            ]
+                        },
+                        {
+                            icon: 'pi pi-fw pi-bars',
+                            label: 'List'
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            label: 'Events',
+            icon: 'pi pi-fw pi-calendar',
+            items: [
+                {
+                    label: 'Edit',
+                    icon: 'pi pi-fw pi-pencil',
+                    items: [
+                        {
+                            label: 'Save',
+                            icon: 'pi pi-fw pi-calendar-plus'
+                        },
+                        {
+                            label: 'Delete',
+                            icon: 'pi pi-fw pi-calendar-minus'
+                        }
+                    ]
+                },
+                {
+                    label: 'Archieve',
+                    icon: 'pi pi-fw pi-calendar-times',
+                    items: [
+                        {
+                            label: 'Remove',
+                            icon: 'pi pi-fw pi-calendar-minus'
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            label: 'Quit',
+            icon: 'pi pi-fw pi-power-off'
+        }
+    ];
 
     public async ngOnInit() {
         forkJoin([
-            
+            await this.getHeaderItemsAsync()
         ]).subscribe();
+
+        this.isHideAuthButtons = localStorage["t_n"] ? true : false;        
     };
 
     /**
-  * Функция получает список проектов пользователя.
-  * @returns - Список проектов.
+  * Функция получает список элементов меню хидера (верхнее меню).
+  * @returns - Список элементов.
   */
-    // private async getUseProjectsAsync() {
-    //     (await this._projectManagmentService.getUseProjectsAsync())
-    //         .subscribe(_ => {
-    //             console.log("Проекты пользователя для УП: ", this.userProjects$.value.userProjects);
-    //         });
-    // };
+    private async getHeaderItemsAsync() {
+        (await this._projectManagmentService.getHeaderItemsAsync())
+            .subscribe(_ => {
+                console.log("Хидер УП: ", this.headerItems$.value);
+            });
+    };
 }
