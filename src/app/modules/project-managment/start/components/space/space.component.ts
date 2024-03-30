6ab9124fe15fe929bff3a2e051e9e9c602a79cf5
barakov14 <playmagma4@gmail.com>
2024-03-30 21:12:24 +0500

    #31473332 Выдвижной панель скрывается, если пользватель уводить курсор из зоны панели

--- src/app/modules/project-managment/start/components/space/space.component.ts
+++ src/app/modules/project-managment/start/components/space/space.component.ts
@@ -182,4 +182,9 @@ export class SpaceComponent implements OnInit {
       console.log("onSelectPanelMenu");
       this._projectManagmentService.isLeftPanel = true;
     };
+
+    public onClosePanelMenu() {
+      console.log("onClosePanelMenu");
+      this._projectManagmentService.isLeftPanel = false;
+    };
 }
