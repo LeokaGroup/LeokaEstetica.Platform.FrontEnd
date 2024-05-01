29f0dd478230bb8bbe6a2efe91a3973edc78412c
Timoshenko A.I <sierra_93@mail.ru>
2024-05-02 00:05:36 +0300

    #30514045 Изменил нейминг ендпоинта.

--- src/app/modules/project-managment/services/project-managment.service.ts
+++ src/app/modules/project-managment/services/project-managment.service.ts
@@ -795,7 +795,7 @@ export class ProjectManagmentService {
    * @param projectId - Id проекта.
    */
   public async getSprintsAsync(projectId: number) {
-    return await this._http.get(this.apiUrl + `/sprint/backlog-sprints?projectId=${projectId}`).pipe(
+    return await this._http.get(this.apiUrl + `/sprints/sprint-list?projectId=${projectId}`).pipe(
       tap(data => this.sprints.next(data))
     );
   };
