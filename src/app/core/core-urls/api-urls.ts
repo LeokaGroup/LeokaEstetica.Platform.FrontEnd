5d3cc0d127184eb83a7e7bcfd590a8f7208c8558
Timoshenko A.I <sierra_93@mail.ru>
2024-06-11 01:23:26 +0300

    Фикс ендпоинтов.

--- src/app/core/core-urls/api-urls.ts
+++ src/app/core/core-urls/api-urls.ts
@@ -3,14 +3,14 @@
  */
 export const API_URL = {
     apiUrl: window.location.href.includes("https://leoka-estetica-dev.ru") || window.location.href.includes("http://localhost:4200/")
-        // ? "https://leoka-estetica-dev.ru.net"
-        ? "http://localhost:9992"
+        ? "https://leoka-estetica-dev.ru.net"
+        // ? "http://localhost:9992"
         : window.location.href.includes("https://leoka-estetica-test.ru")
             ? "https://leoka-estetica-test.ru.net"
             : "https://leoka-estetica.ru.net",
     apiUrlProjectManagment: window.location.href.includes("https://leoka-estetica-dev.ru") || window.location.href.includes("http://localhost:4200/")
-    // ? "https://leoka-estetica-dev-project-management.ru"
-    ? "http://localhost:9995"
+    ? "https://leoka-estetica-dev-project-management.ru"
+    // ? "http://localhost:9995"
     : window.location.href.includes("https://leoka-estetica-test.ru")
         ? "https://leoka-estetica-test-project-management.ru"
         : "https://leoka-estetica-project-management.ru"
