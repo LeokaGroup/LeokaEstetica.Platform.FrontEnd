8a307632a28706f5f881870bf05ca033e0dbb831
HamSilver <kolpakoff@gmail.com>
2024-08-21 13:18:49 +0200

    #35464364 Убрал выбор типа приглашения пользователя

--- src/app/modules/project/detail/components/detail.component.ts
+++ src/app/modules/project/detail/components/detail.component.ts
@@ -93,12 +93,6 @@ export class DetailProjectComponent implements OnInit {
     isVisibleActionAddProjectArchive: boolean = false;
     isVisibleDeleteButton: boolean = false;
     isProjectInvite: boolean = false;
-    aProjectInviteVarians: any[] = [
-        // { name: 'По ссылке', key: 'Link' },
-        { name: 'По почте', key: 'Email' },
-        // { name: 'По номеру телефона', key: 'PhoneNumber' },
-        { name: 'По логину', key: 'Login' }
-    ];
     selectedInviteVariant: any;
     isVacancyInvite: boolean = false;
     availableAttachVacancies: any[] = [];
@@ -541,11 +535,12 @@ export class DetailProjectComponent implements OnInit {
      * Функция отправляет приглашение в команду проекта пользователю.
      */
     public async onSendInviteProjectTeamAsync() {
-        let inviteProjectTeamMemberInput = new InviteProjectTeamMemberInput();
-        inviteProjectTeamMemberInput.ProjectId = this.projectId;
-        inviteProjectTeamMemberInput.InviteText = this.selectedInviteUser;
-        inviteProjectTeamMemberInput.VacancyId = !this.isVacancyInvite ? this.selectedInviteVacancy.vacancyId : null;
-        inviteProjectTeamMemberInput.InviteType = this.selectedInviteVariant.key;
+        const inviteProjectTeamMemberInput: InviteProjectTeamMemberInput = {
+            ProjectId: +this.projectId,
+            InviteText: this.selectedInviteUser,
+            VacancyId: !this.isVacancyInvite ? this.selectedInviteVacancy.vacancyId : null,
+            InviteType: 'Email'
+        };
 
         (await this._projectService.sendInviteProjectTeamAsync(inviteProjectTeamMemberInput))
         .subscribe(async (response: any) => {
