c531b5b893c481c2e1d7ad67b0f3437753216b40
petrov-andrey-dev <petrov.a.v-14@yandex.ru>
2024-09-16 19:52:54 +0300

    #38014182 Восстановлена логика фильтрации в каталоге проектов

--- src/app/modules/project/catalog/components/catalog.component.ts
+++ src/app/modules/project/catalog/components/catalog.component.ts
@@ -26,21 +26,17 @@ export class CatalogProjectsComponent implements OnInit {
 
     selectedDate: any;
     isAnyVacancies: boolean = false;
-    selectedStage: any;
+    selectedStage: any[] = [];
     // searchText: string = "";
     aProjectsCatalog: any[] = [];
     page: number = 0;
     rowsCount: number = 0;
     lastId?: number | null;
     vacancyOptions = [
-      { label: 'Только с открытыми', value: true },
-      { label: 'Все проекты', value: false }
+      { label: 'С наличием вакансий', value: true }
     ];
     dateOptions = [
-      { label: 'Любое время', value: 'all' },
-      { label: 'За месяц', value: 'month' },
-      { label: 'За неделю', value: 'week' },
-      { label: 'За 2 дня', value: '2days' }
+      { label: 'По дате', value: 'Date' }
     ];
     stageOptions = [
       { label: 'Идея', value: 'Concept' },
@@ -53,120 +49,94 @@ export class CatalogProjectsComponent implements OnInit {
       { label: 'Поддержка', value: 'Support' }
     ];
 
-  public async ngOnInit() {
-    this.checkUrlParams();
-    await this.onLoadCatalogProjectsAsync();
-    await this.initVacanciesPaginationAsync();
-  };
+    public async ngOnInit() {
+      this.checkUrlParams();
 
-    private setUrlParams(page: number) {
-        this._router.navigate(["/projects"], {
-            queryParams: {
-                page: page
-            }
-        });
+      await this.onGetCatalogProjectsAsync(null);
     };
 
-    private checkUrlParams() {
-        this._activatedRoute.queryParams
-        .subscribe(params => {
-            console.log("params: ", params);
-            this.page = params["page"]
-            console.log("page: ", this.page);
+      private setUrlParams(page: number) {
+          this._router.navigate(["/projects"], {
+              queryParams: {
+                  page: page
+              }
           });
-    };
+      };
 
-     /**
-     * Функция загружает список вакансий для каталога.
-     * @returns - Список вакансий.
-     */
-     public async onLoadCatalogProjectsAsync() {
-       let catalogProjectInput = new CatalogProjectInput();
-       catalogProjectInput.date = this.selectedDate ? this.selectedDate[0].value : "None";
-
-       if (this.selectedStage) {
-         catalogProjectInput.stageValues = this.selectedStage
-       }
-
-       catalogProjectInput.isAnyVacancies = this.isAnyVacancies;
-       catalogProjectInput.paginationRows = 20;
-       catalogProjectInput.lastId = this.lastId;
-
-       (await this._projectService.loadCatalogProjectsAsync(catalogProjectInput))
-         .subscribe(_ => {
-           console.log("Список проектов: ", this.catalog$.value);
-
-           this.aProjectsCatalog = [];
-           this.rowsCount = 0;
-           this.lastId = 0;
-           this.aProjectsCatalog = this.catalog$.value.catalogProjects;
-           this.rowsCount = this.catalog$.value.total;
-           this.lastId = this.catalog$.value.lastId;
-         });
-     };
-
-    /**Функция сброса фильтров. */
-    // public async onResetFiltersAsync() {
-    //   this.selectedDate = null;
-    //   this.isAnyVacancies = false;
-    //   this.selectedStage = null;
-
-    //   await this.onLoadCatalogProjectsAsync();
-    // }
-
-    /**
-     * Функция переходит к проекту, который выбрали.
-     * @param projectId - Id проекта.
-     */
-    public async onRouteSelectedProject(projectId: number) {
-        this._router.navigate(["/projects/project"], {
-            queryParams: {
-                projectId,
-                mode: "view"
-            }
-        });
-    };
-
-    /**
-     * Функция ищет проекты по поисковому запросу.
-     * @param searchText - Поисковая строка.
-     * @returns - Список проектов после поиска.
-     */
-    // public async onSearchProjectsAsync(event: any) {
-    //   this.searchText = event.query;
-    //   (await this._projectService.searchProjectsAsync(event.query))
-    //     .subscribe(_ => {
-    //       console.log("Результаты поиска: ", this.catalog$.value);
-
-    //       this.aProjectsCatalog = [];
-    //       this.rowsCount = 0;
-    //       this.lastId = 0;
-    //       this.catalog$.value.total = null;
-    //       this.rowsCount = this.catalog$.value.catalogProjects.length;
-    //       this.lastId = this.catalog$.value.lastId;
-    //       this.aProjectsCatalog = this.catalog$.value.catalogProjects;
-    //     });
-    // };
-
-    /**
-     * Функция пагинации проектов.
-     * @param page - Номер страницы.
-     * @returns - Список проектов.
-     */
-     public async onGetProjectsPaginationAsync(event: any) {
-      this.setUrlParams(event.page + 1); // Надо инкрементить, так как event.page по дефолту имеет 0 для 1 элемента.
-      this.page = +this.page + 1;
-      await this.onLoadCatalogProjectsAsync();
-    };
-
-    /**
-     * Функция инициализации пагинации.
-     */
-     private async initVacanciesPaginationAsync() {
-        (await this._projectService.getProjectsPaginationAsync(0))
-            .subscribe(_ => {
-                console.log("Пагинация: ", this.pagination$.value), "page: " + this.page;
-                this.setUrlParams(1);
+      private checkUrlParams() {
+          this._activatedRoute.queryParams
+          .subscribe(params => {
+              console.log("params: ", params);
+              this.page = params["page"]
+              console.log("page: ", this.page);
             });
-    };
+      };
+
+       /**
+       * Функция загружает список проектов для каталога.
+        * Также применяет поиск и пагинацию, если они задействуются.
+       * @returns - Список проектов.
+       */
+       public async onGetCatalogProjectsAsync(event: any) {
+         let catalogProjectInput = new CatalogProjectInput();
+         catalogProjectInput.date = this.selectedDate ? this.selectedDate[0].key : "None";
+
+         if (this.selectedStage) {
+           catalogProjectInput.stageValues = this.selectedStage.join(',');
+         }
+
+         catalogProjectInput.isAnyVacancies = this.isAnyVacancies;
+         catalogProjectInput.paginationRows = 20;
+         catalogProjectInput.lastId = this.lastId;
+
+         // Если используем пагинацию на ините.
+         if (this.page == 0 && event !== null) {
+           this.setUrlParams(event.page + 1); // Надо инкрементить, так как event.page по дефолту имеет 0 для 1 элемента.
+         }
+
+         // Если используем пагинацию после 1 страницы.
+         else if (event !== null) {
+           this.page = +this.page + 1;
+         }
+
+         // Если используем поиск.
+        //  if (event !== null && event.query) {
+        //    this.searchText = event.query;
+        //    catalogProjectInput.searchText = this.searchText;
+        //  }
+
+         (await this._projectService.loadCatalogProjectsAsync(catalogProjectInput))
+           .subscribe(_ => {
+             console.log("Список проектов: ", this.catalog$.value);
+
+             this.aProjectsCatalog = [];
+             this.rowsCount = 0;
+             this.lastId = 0;
+             this.aProjectsCatalog = this.catalog$.value.catalogProjects;
+             this.rowsCount = this.catalog$.value.total;
+             this.lastId = this.catalog$.value.lastId;
+           });
+       };
+
+      /**Функция сброса фильтров. */
+      // public async onResetFiltersAsync() {
+      //   this.selectedDate = null;
+      //   this.isAnyVacancies = false;
+      //   this.selectedStage = null;
+
+      //   await this.onGetCatalogProjectsAsync(null);
+      // }
+
+      /**
+       * Функция переходит к проекту, который выбрали.
+       * @param projectId - Id проекта.
+       */
+      public async onRouteSelectedProject(projectId: number) {
+          this._router.navigate(["/projects/project"], {
+              queryParams: {
+                  projectId,
+                  mode: "view"
+              }
+          });
+      };
 }
