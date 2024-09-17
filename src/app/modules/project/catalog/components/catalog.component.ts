7e516a031c180994f4783173172f9a756240bd83
petrov-andrey-dev <petrov.a.v-14@yandex.ru>
2024-09-17 07:51:09 +0300

    #38014182 исправлены пробелы в catalog.component.ts

--- src/app/modules/project/catalog/components/catalog.component.ts
+++ src/app/modules/project/catalog/components/catalog.component.ts
@@ -26,12 +26,13 @@ export class CatalogProjectsComponent implements OnInit {
 
     selectedDate: any;
     isAnyVacancies: boolean = false;
-    selectedStage: any[] = [];
+    selectedStage: any[] | null = [];
     // searchText: string = "";
     aProjectsCatalog: any[] = [];
     page: number = 0;
     rowsCount: number = 0;
     lastId?: number | null;
+    searchText: string = "";
     vacancyOptions = [
       { label: 'С наличием вакансий', value: true }
     ];
@@ -72,60 +73,60 @@ export class CatalogProjectsComponent implements OnInit {
           });
     };
 
-      /**
+     /**
      * Функция загружает список проектов для каталога.
       * Также применяет поиск и пагинацию, если они задействуются.
-      * @returns - Список проектов.
-      */
-      public async onGetCatalogProjectsAsync(event: any) {
-        let catalogProjectInput = new CatalogProjectInput();
-        catalogProjectInput.date = this.selectedDate ? this.selectedDate[0].key : "None";
+     * @returns - Список проектов.
+     */
+     public async onGetCatalogProjectsAsync(event: any) {
+       let catalogProjectInput = new CatalogProjectInput();
+       catalogProjectInput.date = this.selectedDate ? this.selectedDate[0].key : "None";
 
-        if (this.selectedStage) {
-          catalogProjectInput.stageValues = this.selectedStage.join(',');
-        }
+       if (this.selectedStage) {
+         catalogProjectInput.stageValues = this.selectedStage.join(',');
+       }
 
-        catalogProjectInput.isAnyVacancies = this.isAnyVacancies;
-        catalogProjectInput.paginationRows = 20;
-        catalogProjectInput.lastId = this.lastId;
+       catalogProjectInput.isAnyVacancies = this.isAnyVacancies;
+       catalogProjectInput.paginationRows = 20;
+       catalogProjectInput.lastId = this.lastId;
 
-        // Если используем пагинацию на ините.
-        if (this.page == 0 && event !== null) {
-          this.setUrlParams(event.page + 1); // Надо инкрементить, так как event.page по дефолту имеет 0 для 1 элемента.
-        }
+       // Если используем пагинацию на ините.
+       if (this.page == 0 && event !== null) {
+         this.setUrlParams(event.page + 1); // Надо инкрементить, так как event.page по дефолту имеет 0 для 1 элемента.
+       }
 
-        // Если используем пагинацию после 1 страницы.
-        else if (event !== null) {
-          this.page = +this.page + 1;
-        }
+       // Если используем пагинацию после 1 страницы.
+       else if (event !== null) {
+         this.page = +this.page + 1;
+       }
 
-        // Если используем поиск.
-      //  if (event !== null && event.query) {
-      //    this.searchText = event.query;
-      //    catalogProjectInput.searchText = this.searchText;
-      //  }
+       // Если используем поиск.
+       if (event !== null && event.query) {
+         this.searchText = event.query;
+         catalogProjectInput.searchText = this.searchText;
+       }
 
-        (await this._projectService.loadCatalogProjectsAsync(catalogProjectInput))
-          .subscribe(_ => {
-            console.log("Список проектов: ", this.catalog$.value);
+       (await this._projectService.loadCatalogProjectsAsync(catalogProjectInput))
+         .subscribe(_ => {
+           console.log("Список проектов: ", this.catalog$.value);
 
-            this.aProjectsCatalog = [];
-            this.rowsCount = 0;
-            this.lastId = 0;
-            this.aProjectsCatalog = this.catalog$.value.catalogProjects;
-            this.rowsCount = this.catalog$.value.total;
-            this.lastId = this.catalog$.value.lastId;
-          });
-      };
+           this.aProjectsCatalog = [];
+           this.rowsCount = 0;
+           this.lastId = 0;
+           this.aProjectsCatalog = this.catalog$.value.catalogProjects;
+           this.rowsCount = this.catalog$.value.total;
+           this.lastId = this.catalog$.value.lastId;
+         });
+     };
 
     /**Функция сброса фильтров. */
-    // public async onResetFiltersAsync() {
-    //   this.selectedDate = null;
-    //   this.isAnyVacancies = false;
-    //   this.selectedStage = null;
+    public async onResetFiltersAsync() {
+      this.selectedDate = null;
+      this.isAnyVacancies = false;
+      this.selectedStage = null;
 
-    //   await this.onGetCatalogProjectsAsync(null);
-    // }
+      await this.onGetCatalogProjectsAsync(null);
+    }
 
     /**
      * Функция переходит к проекту, который выбрали.
@@ -136,7 +137,7 @@ export class CatalogProjectsComponent implements OnInit {
             queryParams: {
                 projectId,
                 mode: "view"
-              }
-          });
-      };
+            }
+        });
+    };
 }
