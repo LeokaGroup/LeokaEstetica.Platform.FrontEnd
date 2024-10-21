57b334aa57aa3bbcf71e328b6bac1912917731cf
KirillDemyanenko <demyanenkokg@gmail.com>
2024-10-21 18:17:25 +0300

    40287171 Добавил интерфейс SearchProjectResponseObject

---
+++ src/app/modules/backoffice/my-space/search-project-response-object.ts
@@ -0,0 +1,14 @@
+interface Workspace {
+  "workSpaceId": number,
+  "projectId": number,
+  "projectManagementName": string,
+  "isOwner": boolean,
+  "isSetupProjectNameAndPrefix": boolean,
+  "companyId": number,
+  "companyName": string
+}
+
+export default interface SearchProjectResponseObject {
+  "userCompanyWorkSpaces": Workspace[],
+  "otherCompanyWorkSpaces": Workspace[],
+}
